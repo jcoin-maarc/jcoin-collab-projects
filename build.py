93a403b0b42589cbb4d60c357e519bcb2b333d1f
Michael Kranz <mbkranz@gmail.com>
2023-07-26 21:05:52 -0500

    built out rest of schema (TODO: need to encode individual measures here)

--- build.py
+++ build.py
@@ -7,21 +7,23 @@ from pathlib import Path
 
 # get promis time points schema (only with derived variables) from dictionary
 promis_schema = yaml.safe_load(Path("schemas/dictionary/promis.yaml").read_text())
-modules = ["Record and linkage", "PROMIS 29+2/ PROPr"]
+promis_modules = ["Record and linkage", "PROMIS 29+2/ PROPr"]
 promis_schema["fields"] = [
     dict(OrderedDict(field))
     for field in schemas.timepoints["fields"]
-    if field["module"] in modules
+    if field["module"] in promis_modules
 ] + promis_schema["fields"]
 
 
 # baseline schema
 baseline_schema = dict(schemas.baseline)
+baseline_modules = ["Record and linkage","Demographics"]
 excluded_names = ["quarter_enrolled"]
 baseline_schema["fields"] = [
     dict(OrderedDict(field))
     for field in schemas.baseline["fields"]
-    if not field["name"] in excluded_names
+    if not field["name"] in excluded_names 
+        and field["module"] in baseline_modules
 ]
 
 # write to json
